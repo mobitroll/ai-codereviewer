784cc55d5dbbc1af8ef53423bb24d6833a4941e4
Aymeric Gaurat-Apelli <aymeric@gaurat.net>
2024-10-02 15:38:38 +1000

    Allows to set OPENAI_JSON_MODE

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import minimatch from "minimatch";
 const GITHUB_TOKEN: string = core.getInput("GITHUB_TOKEN");
 const OPENAI_API_KEY: string = core.getInput("OPENAI_API_KEY");
 const OPENAI_API_MODEL: string = core.getInput("OPENAI_API_MODEL");
+const OPENAI_JSON_MODE: string = core.getInput("OPENAI_JSON_MODE");
 
 const octokit = new Octokit({ auth: GITHUB_TOKEN });
 
@@ -127,7 +128,7 @@ async function getAIResponse(prompt: string): Promise<Array<{
     const response = await openai.chat.completions.create({
       ...queryConfig,
       // return JSON if the model supports it:
-      ...(OPENAI_API_MODEL === "gpt-4-1106-preview"
+      ...(OPENAI_JSON_MODE === "true"
         ? { response_format: { type: "json_object" } }
         : {}),
       messages: [
